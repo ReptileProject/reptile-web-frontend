34cf01e552ac8e43525de3534694dc7087f4a0d2
yeochi1201 <yeochi1201@gmail.com>
2025-06-10 00:05:39 +0900

    fix/install axios library

---
+++ Reptile-Project/src/components/creature/SearchBarComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="search-container">
+        <div class="">
+            <!--search by name-->
+            <input type="text" id="speciesName" placeholder="Search Species By Name" />
+        </div>
+        <div class="">
+            <!--search by species group-->
+        </div>
+        <div class="">
+            <!--search by habitat area-->
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
